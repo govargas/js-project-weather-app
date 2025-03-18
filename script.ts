b38b396fdfd35a372f5feff154f345706cc5d8f0
christina-baldwin <christina.baldwin13@yahoo.com>
2025-03-18 14:41:18 +0100

    setting up the api call

--- script.ts
+++ script.ts
@@ -1,59 +1,44 @@
 // Selectors
-const container = document.querySelector(".container");
-const APIKey: string = "2259f103ffcc8ead06b941ec5efcf06e"
-let city: string = "stockholm"
-const weatherURL:string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+const container = document.querySelector(".container")!;
+const APIKey: string = "2259f103ffcc8ead06b941ec5efcf06e";
+let city: string = "stockholm";
+let weatherData;
+const weatherURL: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`;
 
-// Data reference
-let weatherData = {
-  "coord":{"lon":18.0649,"lat":59.3326},
-  "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations",
-  "main":{"temp":281.33,
-    "feels_like":278.02,
-    "temp_min":281.01,
-    "temp_max":281.62,
-    "pressure":1020,
-    "humidity":55,
-    "sea_level":1020,
-    "grnd_level":1015},
-  "visibility":10000,
-  "wind":{"speed":6.17,"deg":290},
-  "clouds":{"all":0},
-  "dt":1742292721,
-  "sys":{"type":1,"id":1788,"country":"SE",
-    "sunrise":1742273722,
-    "sunset":1742316975},
-  "timezone":3600,
-  "id":2673730,
-  "name":"Stockholm",
-  "cod":200
-  }
+const fetchWeatherData = async (): Promise<any> => {
+  const response = await fetch(weatherURL);
+  const data = await response.json();
+  console.log(data);
+  weatherData = data;
+  loadWeatherData(weatherData);
+};
 
-  
+fetchWeatherData();
+
 // LOAD LANDING PAGE DATA
-container.innerHTML = `
-  <div class="landing-page-container">
-    <div class="image-container">
-      <img src="" alt="">
-    </div>
-    <h1>${weatherData.main.temp}</h1>
-    <h2>City</h2>
-    <h3>Weather</h3>
-    <div class="sunrise-sunset">
-      <div class="sunrise-container">
-        <p>Sunrise</p>
-        <p>Time</p>
+const loadWeatherData = (data: any) => {
+  container.innerHTML = `
+    <div class="landing-page-container">
+      <div class="image-container">
+        <img src="" alt="">
       </div>
-      <div class="sunset-container">
-        <p>Sunset</p>
-        <p>Time</p>
+      <h1>${data.main.temp}</h1>
+      <h2>City</h2>
+      <h3>Weather</h3>
+      <div class="sunrise-sunset">
+        <div class="sunrise-container">
+          <p>Sunrise</p>
+          <p>Time</p>
+        </div>
+        <div class="sunset-container">
+          <p>Sunset</p>
+          <p>Time</p>
+        </div>
       </div>
-    </div>
-    <button>&#x3e;</button>
-  </div>`
+      <button>&#x3e;</button>
+    </div>`;
+};
 
-  // tsc file.ts 
+// {"coord":{"lon":18.0649,"lat":59.3326},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"stations","main":{"temp":9.21,"feels_like":8.08,"temp_min":8.95,"temp_max":9.58,"pressure":1020,"humidity":56,"sea_level":1020,"grnd_level":1015},"visibility":10000,"wind":{"speed":2.24,"deg":293,"gust":4.02},"clouds":{"all":47},"dt":1742293894,"sys":{"type":2,"id":2012100,"country":"SE","sunrise":1742273722,"sunset":1742316975},"timezone":3600,"id":2673730,"name":"Stockholm","cod":200}
 
-  // tsc file.ts --outFile file.js
-
-  // tsc --init
+// document.addEventListener("DOMContentLoaded", fetchWeatherData);
