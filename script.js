6b624814bb3f4bb21d11739d0e3913015d3d02ca
Bianka R <bianka_romero@me.com>
2025-03-19 10:04:45 +0100

    getting local time from unix, day/night-time code based on time, updates to innerHTML to display fetched data

--- script.js
+++ script.js
@@ -159,3 +159,91 @@ menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("open");
     navbar.classList.toggle("open");
 });
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+// Selectors
+var container = document.querySelector(".container");
+var APIKey = "2259f103ffcc8ead06b941ec5efcf06e";
+var city = "stockholm";
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=".concat(city, "&units=metric&appid=").concat(APIKey);
+console.log(weatherURL);
+var weatherData;
+// Data reference
+// { "coord": { "lon": 18.0649, "lat": 59.3326 }, "weather": [{ "id": 802, "main": "Clouds", "description": "scattered clouds", "icon": "03d" }], "base": "stations", "main": { "temp": 9.21, "feels_like": 8.08, "temp_min": 8.95, "temp_max": 9.58, "pressure": 1020, "humidity": 56, "sea_level": 1020, "grnd_level": 1015 }, "visibility": 10000, "wind": { "speed": 2.24, "deg": 293, "gust": 4.02 }, "clouds": { "all": 47 }, "dt": 1742293894, "sys": { "type": 2, "id": 2012100, "country": "SE", "sunrise": 1742273722, "sunset": 1742316975 }, "timezone": 3600, "id": 2673730, "name": "Stockholm", "cod": 200 }
+// Unix to Time of Day Function
+var getLocalHours = function (unixTime) { return new Date(unixTime * 1000).getHours(); };
+// Style Page by Time of Day (ToD)
+var updatePageStyle = function () {
+    var currentHour = getLocalHours(weatherData.dt);
+    var sunriseHour = getLocalHours(weatherData.sys.sunrise);
+    var sunsetHour = getLocalHours(weatherData.sys.sunset);
+    var isDaytime = currentHour >= sunriseHour && currentHour < sunsetHour;
+    document.body.classList.add(isDaytime ? "daytime" : "nighttime");
+    console.log("Current Hour: ".concat(currentHour, ", Sunrise Hour: ").concat(sunriseHour, ", Sunset Hour: ").concat(sunsetHour));
+};
+// Fetch API
+var fetchWeatherData = function () { return __awaiter(_this, void 0, void 0, function () {
+    var response, data, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 3, , 4]);
+                return [4 /*yield*/, fetch(weatherURL)];
+            case 1:
+                response = _a.sent();
+                return [4 /*yield*/, response.json()];
+            case 2:
+                data = _a.sent();
+                console.log(data);
+                weatherData = data;
+                loadWeatherData(weatherData);
+                updatePageStyle(weatherData);
+                return [3 /*break*/, 4];
+            case 3:
+                error_1 = _a.sent();
+                console.error("Something went wrong", error_1);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+// LOAD LANDING PAGE DATA
+var loadWeatherData = function () {
+    container.innerHTML += "\n  <div class=\"landing-page-container\">\n    <div class=\"image-container\">\n      <img src=\"\" alt=\"\">\n    </div>\n    <h1>".concat(weatherData.main.temp, "\u00B0 C</h1>\n    <h2>").concat(weatherData.name, "</h2>\n    <h3>").concat(weatherData.weather[0].description, "</h3>\n    <div class=\"sunrise-sunset\">\n      <div class=\"sunrise-container\">\n        <p><b>Sunrise</b></p>\n        <p>").concat(new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(), "</p>\n      </div>\n      <div class=\"sunset-container\">\n        <p>Sunset</p>\n        <p>").concat(new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(), "/p>\n      </div>\n    </div>\n    <button>&#x3e;</button>\n  </div>");
+};
+fetchWeatherData();
